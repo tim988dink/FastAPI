6bde9b8a32fd21881e871af2afa743058088d745
Timothy Dinkel <tim988dink@gmail.com>
2023-03-08 10:05:34 -0700

    added post to main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+
 
 app = FastAPI()
+
 
 
 @app.get("/")
@@ -11,3 +14,7 @@ def root():
 def get_posts():
     return {"data": "This is your posts"}
 
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"title: {payload['title']} content: {payload['content']}"}
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/timothydinkel/Projects/Python/FastAPI/venv"
+set -gx VIRTUAL_ENV "/home/timd/Projects/Python/FastAPI/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
